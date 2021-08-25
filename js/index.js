8abb095060baa6c129a316fbba8b0b650128a5e1
Mike Rogers <mrogers23458@gmail.com>
2021-08-24 18:04:39 -0700

    updated project page, added materialbox logic for future use.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 const btn = document.getElementById('button');
 const formEl = document.getElementById('form')
+const estimateBtnEl = document.getElementById('download-button')
+const estimateMessageEl = document.getElementById('message')
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+
+estimateBtnEl.addEventListener('click', function(){
+  estimateMessageEl.value = `I'm interested in a FREE estimate for my fencing project. Please contact me ASAP!`
+  console.log('clicked')
+})
 
 formEl.addEventListener('submit', function(event) {
    event.preventDefault();
@@ -10,9 +21,11 @@ formEl.addEventListener('submit', function(event) {
    const templateID = 'contact_form';
 
    emailjs.sendForm(serviceID, templateID, this)
+
     .then(() => {
       btn.value = 'Send';
       alert('Sent!');
+      formEl.reset();
     }, (err) => {
       btn.value = 'Send';
       alert(JSON.stringify(err));
